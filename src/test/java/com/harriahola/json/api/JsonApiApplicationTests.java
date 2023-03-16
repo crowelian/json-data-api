d856c2ee190be98d2f418f6bdc9b27b5df4a0870
crowelian-w <harricgi@gmail.com>
2023-03-16 08:52:53 +0200

    initialize spring boot app

---
+++ src/test/java/com/harriahola/json/api/JsonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harriahola.json.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
